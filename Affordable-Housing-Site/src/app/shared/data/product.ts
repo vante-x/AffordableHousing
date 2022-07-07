8a3cf338f39dca268ab7c977a906f02b70ad5a79
Gary Johnston <garyljohnstonjr@gmail.com>
2022-07-07 09:14:57 -0400

    merged products.ts

--- Affordable-Housing-Site/src/app/shared/data/product.ts
+++ Affordable-Housing-Site/src/app/shared/data/product.ts
@@ -1,38 +1,38 @@
 export class Product {
     id: number;
-    images:string[];
+    images: string[];
     name: string;
     rating: number;
     feedback: number;
-    lat:number;
-    lng:number;
+    lat: number;
+    lng: number;
     color: string;
-    energyRating:string;
-    cirlceSize:number;
+    energyRating: string;
+    cirlceSize: number;
     address: string;
     contactNumber: string;
     utilityEstimate: number;
     energyStar: number;
     buildingSize: number;
     siteEIU: number;
-    gasEmissions:number;
-    url:string;
+    gasEmissions: number;
+    url: string;
     neighborhood: string;
     rentMin: number;
-    rentMax:number;
-    beds:string;
+    rentMax: number;
+    beds: string;
     baths: string;
-    sqrFt:number;
+    sqrFt: number;
     petsAllowed: string;
-    petFee:number;
+    petFee: number;
     applicationFee: number;
     includedCosts: string;
-    yearBuilt:number;
-    description:string;
-    amenities:string;
+    yearBuilt: number;
+    description: string;
+    amenities: string;
     apartmentFeatures: string;
-    image1:string; //not working
-    image2:string;
-    image3:string;
+    image1: string; //not working
+    image2: string;
+    image3: string;
     price: number;
 }
